d96aea3be9b2d383eceabc7cb3973f1ab66d9128
Pacotacou <p4c0taco@gmail.com>
2024-12-29 15:58:38 -0500

    fix: fix unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,71 +1,79 @@
-
 const chai = require('chai');
 const assert = chai.assert;
-const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-suite('Unit Tests', () => {
-  const solver = new SudokuSolver();
+const Solver = require('../controllers/sudoku-solver.js');
+let solver = new Solver();
 
-  test('Logic handles a valid puzzle string of 81 characters', () => {
-    const validPuzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37.';
-    assert.isTrue(solver.validate(validPuzzle), 'The puzzle string should be valid');
-  });
+suite('UnitTests', () => {
 
-  test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', () => {
-    const invalidPuzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....9X6914.37.';
-    assert.isFalse(solver.validate(invalidPuzzle), 'The puzzle string should be invalid due to non-valid characters');
-  });
+  suite('SudokuSolver', () => {
+    // #1
+    test('a valid puzzle string of 81 characters', () => {
+      assert.isTrue(solver.validateLength('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'));
+    });
 
-  test('Logic handles a puzzle string that is not 81 characters in length', () => {
-    const shortPuzzle = '1.5..2.84..63.12.7..2..5....9..1.';
-    const longPuzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37...';
-    assert.isFalse(solver.validate(shortPuzzle), 'The puzzle string should be invalid because it is too short');
-    assert.isFalse(solver.validate(longPuzzle), 'The puzzle string should be invalid because it is too long');
-  });
+    // #2
+    test('a puzzle string with invalid characters (not 1-9 or .)', () => {
+      assert.isNotTrue(solver.validateChar('a..sdfqw.e...8.5.9.9.25..H.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...Z'))
+    });
 
-  test('Logic handles a valid row placement', () => {
-    const puzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37.';
-    assert.isTrue(solver.checkRowPlacement(puzzle, 0, 1, '3'), 'Value 3 should be valid in row 0');
-  });
+    // #3
+    test('a puzzle string that is not 81 characters in length', () => {
+      assert.isNotTrue(solver.validateLength('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37'));
+      assert.isNotTrue(solver.validateLength('5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3..6379..1'));
+    });
 
-  test('Logic handles an invalid row placement', () => {
-    const puzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37.';
-    assert.isFalse(solver.checkRowPlacement(puzzle, 0, 1, '1'), 'Value 1 should be invalid in row 0');
-  });
+    // #4
+    test('a valid row placement', () => {
+      assert.isTrue(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'a', 1, 1));
+      assert.isTrue(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'b', 3, 5));
+    });
 
-  test('Logic handles a valid column placement', () => {
-    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-    assert.isFalse(solver.checkColPlacement(puzzle, 0, 2, '9'), 'Value 6 should be valid in column 1');
-  });
+    // #5
+    test('an invalid row placement', () => {
+      assert.isNotTrue(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'a', 1, 4));
+      assert.isNotTrue(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'i', 6, 1));
+    });
 
-  test('Logic handles an invalid column placement', () => {
-    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-    assert.isTrue(solver.checkColPlacement(puzzle, 0, 1, '15'), 'Value 5 should be invalid in column 1');
-  });
+    // #6
+    test('a valid column placement', () => {
+      assert.isTrue(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'a', 1, 5));
+      assert.isTrue(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'B', 3, 8));
+    });
 
-  test('Logic handles a valid region (3x3 grid) placement', () => {
-    const puzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37.';
-    assert.isTrue(solver.checkRegionPlacement(puzzle, 0, 1, '4'), 'Value 4 should be valid in the top-left 3x3 region');
-  });
+    // #7 
+    test('an invalid column placement', () => {
+      assert.isNotTrue(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'i', 7, 2));
+      assert.isNotTrue(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'i', 7, 7));
+    });
 
-  test('Logic handles an invalid region (3x3 grid) placement', () => {
-    const puzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37.';
-    assert.isFalse(solver.checkRegionPlacement(puzzle, 0, 1, '2'), 'Value 2 should be invalid in the top-left 3x3 region');
-  });
+    // #8
+    test('logic handles a valid region (3x3 grid) placement', () => {
+      assert.isTrue(solver.checkRegionPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'A', 1, 3));
+      assert.isTrue(solver.checkRegionPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'd', 1, 5));
+    });
 
-  test('Valid puzzle strings pass the solver', () => {
-    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-    assert.isString(solver.solve(puzzle), 'The solver should return a solution as a string');
-  });
+    // #9
+    test('an invalid region (3x3 grid) placement', () => {
+      assert.isNotTrue(solver.checkRegionPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'A', 1, 6));
+      assert.isNotTrue(solver.checkRegionPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 'g', 4, 6));
+    });
 
-  test('Invalid puzzle strings fail the solver', () => {
-    const invalidPuzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....9X6914.37.';
-    assert.throws(() => solver.solve(invalidPuzzle), Error, 'Invalid puzzle string', 'The solver should throw an error for invalid puzzle strings');
-  });
+    // #10
+    test('valid puzzle strings pass the solver', () => {
+      assert.isOk(solver.solve('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'));
+      assert.isOk(solver.solve('5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3'));
+    });
 
-  test('Solver returns the expected solution for an incomplete puzzle', () => {
-    const puzzle = '1.5..2.84..63.12.7..2..5....9..1....8.2.3674.3.7.2..9..47..8..1..16....926914.37.';
-    const solution = '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
-    assert.throws(() => solver.solve(puzzle), Error, 'Puzzle cannot be solved', 'The solver should throw an error for incomplete puzzle');
+    // #11
+    test('invalid puzzle strings fail the solver', () => {
+      assert.isNotOk(solver.solve('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37'));
+      assert.isNotOk(solver.solve('5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3...567.'));
+    });
+
+    // #12
+    test('solver returns the the expected solution for an incomplete puzzle', () => {
+      assert.equal(solver.solve('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'), '135762984946381257728459613694517832812936745357824196473298561581673429269145378');
+    });
   });
 });
